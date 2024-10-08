9f50b6fc3e14085fc8d3459c306850676ca2c9e3
ryder <a23rijija@iesgrancapitan.org>
2024-10-08 19:11:22 +0200

    funciones terminadas

---
+++ 08incluir_archivos.php
@@ -0,0 +1,16 @@
+<?php
+/*
+para incluir archiivos haay 4 funciones que funcionan casi igual:
+
+-include(): incluye el contenido del argumento en el lugar donde invoce. si el archivo no existe se emite un aviso y el script continua
+
+-require(): incluye el contenido del argumento donde se invoque. si el archivo no existe se emite un error fatal y acaba la ejecucion
+
+-que ocurre si incluyo el mismo archivo mas de una vez?
+
+*/
+include("include/funciones.php");
+
+
+
+?>
