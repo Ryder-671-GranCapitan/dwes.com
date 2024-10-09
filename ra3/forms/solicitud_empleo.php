0039c5deb1b33d7b3d5a30a52bc52c5c5187c32f
ryder <a23rijija@iesgrancapitan.org>
2024-10-09 19:31:16 +0200

    formularios

--- ra3/forms/solicitud_empleo.php
+++ ra3/forms/solicitud_empleo.php
@@ -3,9 +3,14 @@ require($_SERVER['DOCUMENT_ROOT'] . "/includes/funciones.php");
 
 inicio_html("proceso de formulario 1" , ["/style/general.css"]);
 
-$nombre = $_GET['nombre'];
-$email = $_GET['email'];
-$clave = $_GET['clave'];
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$clave = $_POST['clave'];
 
+$perfil_linkedin = $_POST['linkedin'];
+$fecha_disponible = $_POST['fecha_disponible'];
+$hora_entrevista = $_POST['hora_entrevista'];
+$salario = $_POST['salario'];
+$areas = $_POST['areas'];
 
 ?>
