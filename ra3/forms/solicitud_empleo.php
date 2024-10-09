c234bc75cd1759b8d0b21cb82e6b2df57078910a
ryder <a23rijija@iesgrancapitan.org>
2024-10-09 19:08:44 +0200

    Empezamos con formularios

---
+++ ra3/forms/solicitud_empleo.php
@@ -0,0 +1,11 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . "/includes/funciones.php");
+
+inicio_html("proceso de formulario 1" , ["/style/general.css"]);
+
+$nombre = $_GET['nombre'];
+$email = $_GET['email'];
+$clave = $_GET['clave'];
+
+
+?>
